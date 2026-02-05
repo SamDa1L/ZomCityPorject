3749091df412fe2754fe48b7b37cfe96209df1a1
WSX <wsx283848@gmail.com>
2026-02-05 15:09:51 +0800

    模版上传

---
+++ Assets/Julhiecio TPS Controller/Scripts/Gameplay/Character Controllers/Additionals/AimOnRightJoystickDirection.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using JUTPSActions;
+
+using JUTPS.JUInputSystem;
+using JUTPS.CameraSystems;
+
+namespace JUTPS.ActionScripts
+{
+
+    public class AimOnRightJoystickDirection : JUTPSAction
+    {
+        [HideInInspector] public static Vector3 AimPosition;
+        [SerializeField] private JUCameraController cameraController;
+        private float _nextCameraSearchTime;
+
+        [Header("Settings")]
+        public bool Enabled = true;
+        public float DistanceFromCenter = 5;
+        public float UpOffset;
+        public bool FireModeWhenHasJoystickDirection = true;
+        [Header("Aim Mode Settings")]
+        public bool SidescrollerAimMode;
+
+        float Xinput, Yinput;
+
+
+        [HideInInspector] public bool IsUsingJoystick;
+        void Start()
+        {
+            ResolveCameraController();
+        }
+
+        private void ResolveCameraController()
+        {
+            if (cameraController != null) return;
+            if (Time.unscaledTime < _nextCameraSearchTime) return;
+            _nextCameraSearchTime = Time.unscaledTime + 1f;
+
+
+            #if UNITY_6000_0_OR_NEWER || UNITY_2023_1_OR_NEWER
+            cameraController = Object.FindAnyObjectByType<JUCameraController>();
+            #else
+            cameraController = FindObjectOfType<JUCameraController>();
+            #endif
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Enabled == false)
+            {
+                return;
+            }
+            float realXinput = Mathf.Clamp(Mathf.Abs(JUInput.GetAxis(JUInput.Axis.RotateHorizontal)), -1, 1);
+            float realYinput = Mathf.Clamp(Mathf.Abs(JUInput.GetAxis(JUInput.Axis.RotateVertical)), -1, 1);
+
+            if (realYinput > 0.1f || realXinput > 0.1f)
+            {
+                IsUsingJoystick = true;
+                Yinput = JUInput.GetAxis(JUInput.Axis.RotateVertical);
+                Xinput = JUInput.GetAxis(JUInput.Axis.RotateHorizontal);
+                if (FireModeWhenHasJoystickDirection)
+                {
+                    if (TPSCharacter.HoldableItemInUseRightHand == null)
+                    {
+                        TPSCharacter.CurrentTimeToDisableFireMode = 0;
+                        TPSCharacter.FiringMode = true;
+                        TPSCharacter.FiringModeIK = true;
+                    }
+                    else
+                    {
+                        if (TPSCharacter.HoldableItemInUseRightHand.BlockFireMode == false)
+                        {
+                            TPSCharacter.CurrentTimeToDisableFireMode = 0;
+                            TPSCharacter.FiringMode = true;
+                            TPSCharacter.FiringModeIK = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (FireModeWhenHasJoystickDirection && IsUsingJoystick)
+                {
+                    TPSCharacter.FiringMode = false; TPSCharacter.FiringModeIK = false;
+                    IsUsingJoystick = false;
+                }
+            }
+
+            if (SidescrollerAimMode)
+            {
+                Vector3 Direction = new Vector3(Xinput, Yinput);
+
+                //Modify position
+                //Direction.z = Mathf.Lerp(TPSCharacter.transform.position.z - 3f, Yinput, new Vector3(Xinput, Yinput).magnitude + 0.5f);
+
+                AimPosition = TPSCharacter.PivotItemRotation.transform.position + Direction.normalized * DistanceFromCenter;
+                TPSCharacter.LookAtPosition = AimPosition;
+            }
+            else
+            {
+                Vector3 Direction = new Vector3(Xinput, 0, Yinput).normalized;
+                if (cameraController == null)
+                {
+                    ResolveCameraController();
+                }
+
+                var cam = (cameraController != null && cameraController.mCamera != null) ? cameraController.mCamera : Camera.main;
+                float camYaw = cam != null ? cam.transform.eulerAngles.y : 0f;
+                Quaternion CamDirection = Quaternion.Euler(0, camYaw, 0);
+                Quaternion DesiredDirection = Direction.magnitude > 0 ? Quaternion.LookRotation(Direction, Vector3.up) * CamDirection : Quaternion.identity;
+                Vector3 DesiredAimPosition = TPSCharacter.PivotItemRotation.transform.position + (DesiredDirection * Vector3.forward) * DistanceFromCenter + transform.up * UpOffset;
+                AimPosition = DesiredAimPosition;
+
+                //AimPosition = TPSCharacter.PivotItemRotation.transform.position + (Direction * DistanceFromCenter) + transform.up * UpOffset;
+                TPSCharacter.LookAtPosition = AimPosition;
+            }
+
+
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            if (SidescrollerAimMode)
+            {
+                Gizmos.DrawWireCube(AimPosition, new Vector3(0.1f, 0.1f, 0));
+                Gizmos.DrawWireCube(AimPosition, new Vector3(0.5f, 0.5f, 0));
+            }
+            else
+            {
+                Gizmos.DrawWireCube(AimPosition, new Vector3(0.1f, 0, 0.1f));
+                Gizmos.DrawWireCube(AimPosition, new Vector3(0.5f, 0, 0.5f));
+            }
+        }
+    }
+}
